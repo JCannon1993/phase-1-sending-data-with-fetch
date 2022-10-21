3ca3a1320705401d1180becdd227a6039ffc1831
Jelani Cannon <jelani.cannon@gmail.com>
2022-10-20 21:06:24 -0700

    commit tree e15ef00e767f141e9589970eeb87889d4c8a3797

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+
+function submitData (name, email){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`,
+        })
+    })
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(object){
+            const body = document.querySelector('body')
+            body.innerHTML = `${object.id}`
+           
+        })
+        .catch(function(error){
+            document.body.innerHTML = error.message
+        })
+    }
+        
+    
+    
+    
+    
+    
+    
+    
+    // const configurationObject = {
+    //     method: "POST",
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //         "Accept": "application/json"
+    //     },
+    //     body: JSON.stringify({
+    //         dogName: 'Byron',
+    //         dogBreed: "Poodle",
+    //     }),
+    // }
+    // fetch("http://localhost:3000/dogs", configurationObject)
+    // .then(function (response){
+    //     return response.json();
+    // })
+    // .then(function (object){
+    //     console.log(object)
+    //})
